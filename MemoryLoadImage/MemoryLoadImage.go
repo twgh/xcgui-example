5722d076ea60c7c0da64aca5a87cb8a7b3b63cea
twgh <twgh521@qq.com>
2021-12-22 14:19:50 +0800

    内存加载图片

--- MemoryLoadImage/MemoryLoadImage.go
+++ MemoryLoadImage/MemoryLoadImage.go
@@ -19,7 +19,7 @@ func main() {
 	w := window.NewWindow(0, 0, 415, 296, "", 0, xcc.Window_Style_Default)
 
 	// 加载图片从内存
-	hImg := xc.XImage_LoadMemory(&img1, len(img1))
+	hImg := xc.XImage_LoadMemory(img1)
 
 	//创建形状对象-图片
 	shapePic := widget.NewShapePicture(8, 30, 400, 260, w.Handle)
