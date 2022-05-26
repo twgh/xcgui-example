a5fbe9897dd9faade059f54b30c672605588b2b5
twgh <twgh521@qq.com>
2022-05-26 21:52:36 +0800

    日期时间框

--- DateTime/DateTime.go
+++ DateTime/DateTime.go
@@ -16,6 +16,10 @@ func main() {
 	// 0为日期元素, 1为时间元素.
 	dt.SetStyle(0)
 
+	dt2 := widget.NewDateTime(20, 150, 120, 26, w.Handle)
+	// 0为日期元素, 1为时间元素.
+	dt2.SetStyle(1)
+
 	w.ShowWindow(xcc.SW_SHOW)
 	a.Run()
 	a.Exit()
