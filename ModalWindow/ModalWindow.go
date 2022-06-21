db23c65f4b86e7728be3f2bd77b224369b65602b
twgh <twgh521@qq.com>
2022-06-21 11:53:00 +0800

    模态窗口

--- ModalWindow/ModalWindow.go
+++ ModalWindow/ModalWindow.go
@@ -9,26 +9,21 @@ import (
 )
 
 func main() {
-	// 1.初始化UI库
 	a := app.New(true)
-	// 2.创建窗口
-	w := window.NewWindow(0, 0, 430, 300, "xc", 0, xcc.Window_Style_Simple|xcc.Window_Style_Btn_Close)
+	w := window.NewWindow(0, 0, 430, 300, "xc", 0, xcc.Window_Style_Default)
 
 	// 创建按钮_模态窗口
 	btn := widget.NewButton(30, 50, 100, 30, "ModalWindow", w.Handle)
 	// 给按钮绑定事件
 	btn.Event_BnClick(func(pbHandled *bool) int {
 		// 创建模态窗口
-		win_Modal := window.NewModalWindow(300, 200, "ModalWindow", w.GetHWND(), xcc.Window_Style_Modal_Simple|xcc.Window_Style_Drag_Window|xcc.Window_Style_Btn_Close)
+		win_Modal := window.NewModalWindow(300, 200, "ModalWindow", w.GetHWND(), xcc.Window_Style_Modal)
 		// 显示模态窗口
 		win_Modal.DoModal()
 		return 0
 	})
 
-	// 3.显示窗口
 	w.ShowWindow(xcc.SW_SHOW)
-	// 4.运行程序
 	a.Run()
-	// 5.释放UI库
 	a.Exit()
 }
