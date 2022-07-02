989dc66475562d87f8577d98d44d1d0274e32ccb
twgh <twgh521@qq.com>
2022-07-02 19:06:10 +0800

    简单窗口

--- SimpleWindow/SimpleWindow.go
+++ SimpleWindow/SimpleWindow.go
@@ -11,7 +11,7 @@ func main() {
 	// 1.初始化UI库
 	a := app.New(true)
 	// 2.创建窗口
-	w := window.NewWindow(0, 0, 430, 300, "", 0, xcc.Window_Style_Simple|xcc.Window_Style_Btn_Close)
+	w := window.New(0, 0, 430, 300, "", 0, xcc.Window_Style_Simple|xcc.Window_Style_Btn_Close)
 	// 设置窗口边框大小
 	w.SetBorderSize(0, 30, 0, 0)
 	// 设置窗口透明类型
