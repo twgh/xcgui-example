a91e6e3a3f288df3a5f0be5df8a88b7eaa782715
twgh <twgh521@qq.com>
2022-07-02 19:06:55 +0800

    线程操作UI

--- ThreadOperationUI/ThreadOperationUI.go
+++ ThreadOperationUI/ThreadOperationUI.go
@@ -3,105 +3,131 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"strconv"
-	"sync"
-	"time"
-
 	"github.com/twgh/xcgui/app"
 	"github.com/twgh/xcgui/widget"
 	"github.com/twgh/xcgui/window"
 	"github.com/twgh/xcgui/xc"
 	"github.com/twgh/xcgui/xcc"
+	"strconv"
+	"time"
 )
 
 var (
-	a   *app.App
-	w   *window.Window
-	btn *widget.Button
-	ls  *widget.List
+	a         *app.App
+	w         *window.Window
+	btn       *widget.Button
+	radioBtn1 *widget.Button
+	radioBtn2 *widget.Button
 
-	rwm sync.RWMutex
-	wg  sync.WaitGroup
+	t = 1 // 方式类型
 )
 
 func main() {
-	// 置随机数种子
-	rand.Seed(time.Now().UnixNano())
-
 	a = app.New(true)
-	w = window.NewWindow(0, 0, 550, 300, "ThreadOperationUI", 0, xcc.Window_Style_Default)
+	w = window.New(0, 0, 550, 300, "ThreadOperationUI", 0, xcc.Window_Style_Default)
 
-	btn = widget.NewButton(15, 33, 70, 24, "click", w.Handle)
+	// 创建按钮
+	btn = widget.NewButton(20, 35, 100, 30, "click", w.Handle)
 	btn.Event_BnClick(onBnClick)
 
-	ls = widget.NewList(10, 60, 530, 230, w.Handle)
-	ls.CreateAdapterHeader() // 创建表头数据适配器
-	ls.CreateAdapter(5)      // 创建数据适配器, 5列
+	// 单选按钮
+	radioBtn1 = widget.NewButton(20, 70, 70, 30, "方式1", w.Handle)
+	radioBtn2 = widget.NewButton(100, 70, 70, 30, "方式2", w.Handle)
+	radioBtn1.SetTypeEx(xcc.Button_Type_Radio)
+	radioBtn2.SetTypeEx(xcc.Button_Type_Radio)
+	radioBtn1.SetCheck(true) // 默认选中radioBtn1
+	radioBtn1.Event_BUTTON_CHECK1(onBnCheck)
+	radioBtn2.Event_BUTTON_CHECK1(onBnCheck)
 
-	ls.AddColumnText(100, "name1", "column1")
-	ls.AddColumnText(100, "name2", "column2")
-	ls.AddColumnText(100, "name3", "column3")
-	ls.AddColumnText(100, "name4", "column4")
-	ls.AddColumnText(100, "name5", "column5")
-
-	for i := 1; i < 10; i++ {
-		id := strconv.Itoa(i)
-		index := ls.AddItemText("item" + id + "-column" + id)
-		ls.SetItemText(index, 1, "item"+id+"-column"+id)
-		ls.SetItemText(index, 2, "item"+id+"-column"+id)
-		ls.SetItemText(index, 3, "item"+id+"-column"+id)
-		ls.SetItemText(index, 4, "item"+id+"-column"+id)
-	}
-
-	w.Show(true)
-	a.Run()
+	a.ShowAndRun(w.Handle)
 	a.Exit()
 }
 
 func onBnClick(pbHandled *bool) int {
-	if !btn.IsEnable() {
-		return 0
-	}
+	// 禁用按钮
 	btn.Enable(false)
 	btn.Redraw(true)
 
-	go func() {
-		t := time.Now() // 记录开始的时间
-
-		// 多线程操作列表框数据
-		for i := 0; i < 2000; i++ {
-			wg.Add(1)
-
-			//go setText() // 这样另起线程操作UI次数多了程序将崩溃
-			go func() {
-				xc.XC_CallUiThread(setText, 0) // 这样是在主线程进行UI操作, 就不会崩溃了
-			}()
-		}
-		wg.Wait() // 等待全部执行完毕
-
-		xc.XC_CallUiThread(func(data int) int {
-			ls.RefreshData() // 刷新列表项数据
-			ls.Redraw(false) // 列表重绘
-			btn.Enable(true)
-			btn.Redraw(true)
-			w.MessageBox("提示", fmt.Sprintf("全部执行完毕, 耗时: %v", time.Since(t)), xcc.MessageBox_Flag_Ok, xcc.Window_Style_Default)
-			return 0
-		}, 0)
-	}()
-
+	// 另起线程是为了不卡界面
+	switch t {
+	case 1:
+		go updateBtn1() // 第一种方式: xc.XC_CallUiThreadEx
+	case 2:
+		go updateBtn2() // 第二种方式: xc.XC_CallUiThreader
+	}
 	return 0
 }
 
-func setText(data int) int {
-	item := rand.Intn(ls.GetCount_AD())
-	col := rand.Intn(ls.GetColumnCount())
-	text := strconv.Itoa(rand.Intn(1000) + 1000)
+// 第一种方式
+func updateBtn1() {
+	fmt.Println("使用方式1: xc.XC_CallUiThreadEx")
+	for i := 0; i < 2010; i++ {
+		// 如果直接在非主线程内操作UI, 次数多了程序必将崩溃, 而且你不会知道它在什么时候崩溃.
+		// 使用 xc.XC_CallUiThreadEx 这样是在界面线程进行UI操作, 就不会崩溃了.
+		xc.XC_CallUiThreadEx(func(data int) int {
+			btn.SetText(strconv.Itoa(data))
+			btn.SetWidth(i / 5)
+			w.Redraw(false)
+			return 0
+		}, i)
+		time.Sleep(time.Millisecond * 1)
+	}
 
-	rwm.RLock()
-	ls.SetItemText(item, col, text)
-	rwm.RUnlock()
+	// 解禁按钮
+	xc.XC_CallUiThreadEx(func(data int) int {
+		btn.Enable(true)
+		btn.Redraw(true)
+		return 0
+	}, 0)
+}
 
-	wg.Done()
+func updateBtn2() {
+	fmt.Println("使用方式2: xc.XC_CallUiThreader")
+	u := updateButton{
+		HEle:         btn.Handle,
+		RedrawWindow: false,
+	}
+
+	for i := 0; i < 2010; i++ {
+		// 如果直接在非主线程内操作UI, 次数多了程序必将崩溃, 而且你不会知道它在什么时候崩溃.
+		// 使用 xc.XC_CallUiThreader 这样是在界面线程进行UI操作, 就不会崩溃了.
+		u.Text = strconv.Itoa(i)
+		u.Width = i / 5
+		xc.XC_CallUiThreader(u, 0)
+		time.Sleep(time.Millisecond * 1)
+	}
+
+	// 解禁按钮
+	xc.XC_CallUiThreadEx(func(data int) int {
+		btn.Enable(true)
+		btn.Redraw(true)
+		return 0
+	}, 0)
+}
+
+type updateButton struct {
+	HEle         int
+	Text         string
+	Width        int
+	RedrawWindow bool
+}
+
+func (u updateButton) UiThreadCallBack(data int) int {
+	xc.XBtn_SetText(u.HEle, u.Text)
+	xc.XEle_SetWidth(u.HEle, u.Width)
+	w.Redraw(u.RedrawWindow)
+	return 0
+}
+
+// 单选按钮被选择
+func onBnCheck(hEle int, bCheck bool, pbHandled *bool) int {
+	if bCheck {
+		switch hEle {
+		case radioBtn1.Handle:
+			t = 1
+		case radioBtn2.Handle:
+			t = 2
+		}
+	}
 	return 0
 }
