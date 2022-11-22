05110a225a8b6a57b7f666300ce2e61f22e17621
twgh <twgh521@qq.com>
2022-11-22 17:17:58 +0800

    设计器使用例子

--- uidesigner/uidesigner.go
+++ uidesigner/uidesigner.go
@@ -5,8 +5,8 @@ import (
 	_ "embed"
 
 	"github.com/twgh/xcgui/app"
+	"github.com/twgh/xcgui/widget"
 	"github.com/twgh/xcgui/window"
-	"github.com/twgh/xcgui/xcc"
 )
 
 //go:embed res/qqmusic.zip
@@ -18,10 +18,17 @@ func main() {
 	a.LoadResourceZipMem(qqmusic, "resource.res", "")
 	// 从内存zip中加载布局文件, 创建窗口对象
 	w := window.NewByLayoutZipMem(qqmusic, "main.xml", "", 0, 0)
+
+	// songTitle是在main.xml中给歌曲名(shapeText组件)设置的name属性的值.
+	// 通过 GetObjectByName 可以获取布局文件中设置了name属性的组件的句柄.
+	// 可简化为: widget.NewShapeTextByName("songTitle").
+	songTitle := widget.NewShapeTextByHandle(a.GetObjectByName("songTitle"))
+	println(songTitle.GetText()) // 输出: 两只老虎爱跳舞
+
 	// 调整布局
 	w.AdjustLayout()
 	// 显示窗口
-	w.ShowWindow(xcc.SW_SHOW)
+	w.Show(true)
 	a.Run()
 	a.Exit()
 }
