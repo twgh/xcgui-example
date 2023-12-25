d7d22d42f7fe38a6e451be4863d489d523bdf159
twgh <twgh521@qq.com>
2023-12-25 22:21:58 +0800

    编辑框

--- Edit/Edit.go
+++ Edit/Edit.go
@@ -3,7 +3,6 @@ package main
 
 import (
 	"fmt"
-
 	"github.com/twgh/xcgui/app"
 	"github.com/twgh/xcgui/widget"
 	"github.com/twgh/xcgui/window"
@@ -13,7 +12,7 @@ import (
 
 func main() {
 	a := app.New(true)
-	w := window.New(0, 0, 430, 300, "", 0, xcc.Window_Style_Default)
+	w := window.New(0, 0, 340, 280, "", 0, xcc.Window_Style_Default)
 
 	// 1.普通编辑框
 	edit := widget.NewEdit(12, 35, 100, 30, w.Handle)
@@ -21,38 +20,46 @@ func main() {
 	edit.SetText("hello")
 
 	// 2.密码输入框
-	edit_pwd := widget.NewEdit(12, 75, 100, 30, w.Handle)
-	edit_pwd.EnablePassword(true)
-	edit_pwd.SetText("pwd")
+	editPwd := widget.NewEdit(12, 75, 100, 30, w.Handle)
+	editPwd.EnablePassword(true)
+	editPwd.SetText("pwd")
 	// 这个可以改变密码字符
-	edit_pwd.SetPasswordCharacter('#')
+	editPwd.SetPasswordCharacter('#')
 
 	// 3.多行编辑框
-	edit_MultiLine := widget.NewEdit(12, 115, 300, 70, w.Handle)
-	edit_MultiLine.EnableMultiLine(true)
-	edit_MultiLine.AddText("你好, 世界")
+	editMultiLine := widget.NewEdit(12, 115, 300, 100, w.Handle)
+	editMultiLine.EnableAutoShowScrollBar(true)
+	editMultiLine.EnableMultiLine(true)
+	editMultiLine.EnableAutoWrap(true)
+	editMultiLine.AddText("你好, 世界")
 	// 添加样式
-	style1 := edit_MultiLine.AddStyleEx("Arial", 14, xcc.FontStyle_Bold, xc.ABGR(0, 191, 165, 255), true)
+	style1 := editMultiLine.AddStyleEx("Arial", 14, xcc.FontStyle_Bold, xc.ABGR(0, 191, 165, 255), true)
 	// 添加带样式的文本
-	edit_MultiLine.AddTextEx("\nhello world", style1)
-
+	editMultiLine.AddTextEx("\nhello world", style1)
 	// 获取编辑框文本
-	var s string
-	edit_MultiLine.GetText(&s, edit_MultiLine.GetLength()+1) // 长度必须+1
-	fmt.Printf("s: %s\n\n", s)
+	fmt.Println("获取编辑框文本:", editMultiLine.GetTextEx())
 
-	// 或者使用封装好的方法 GetTextEx()
-	fmt.Println(edit_MultiLine.GetTextEx())
+	// 设置定时器, 循环获取多行编辑框鼠标选中的文本
+	editMultiLine.SetXCTimer(111, 1000)
+	editMultiLine.Event_XC_TIMER(func(nTimerID int, pbHandled *bool) int {
+		if nTimerID == 111 {
+			text := editMultiLine.GetSelectTextEx()
+			if text != "" {
+				fmt.Println("选中的文本:", text)
+			}
+		}
+		return 0
+	})
 
 	// 4.只能输入数字的编辑框
-	edit4 := widget.NewEdit(12, 195, 100, 30, w.Handle)
-	edit4.Event_CHAR(func(wParam int, lParam int, pbHandled *bool) int {
+	editOnlyNumber := widget.NewEdit(12, 225, 100, 30, w.Handle)
+	editOnlyNumber.Event_CHAR(func(wParam, lParam uint, pbHandled *bool) int {
 		fmt.Println(wParam)
 		if wParam < 58 && wParam > 47 { // 0-9
 			return 0
 		}
 
-		switch wParam { // 删除,复制,撤销,剪切,全选. 有其它需要的自己添加.
+		switch wParam { // 放行删除,复制,撤销,剪切,全选. 有其它需要的自己添加.
 		case 8, 3, 26, 24, 1:
 			return 0
 		}
