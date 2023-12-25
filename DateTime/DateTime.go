2114eb6e953609bc25861ac2cf19dc5bf1104ff5
twgh <twgh521@qq.com>
2023-12-25 22:19:15 +0800

    日期时间框

--- DateTime/DateTime.go
+++ DateTime/DateTime.go
@@ -1,6 +1,6 @@
 // 日期时间框
 // 要美化的话, 就得自绘, 看这个: http://www.xcgui.com/doc-ui/page_draw__month_cal.html
-// 我以后有空会翻译过来
+// 我以后有空会翻译几个好看的: http://mall.xcgui.com/1618.html
 package main
 
 import (
@@ -12,13 +12,15 @@ import (
 
 func main() {
 	a := app.New(true)
-	w := window.New(0, 0, 400, 300, "", 0, xcc.Window_Style_Default)
+	a.EnableDPI(true)
+	a.EnableAutoDPI(true)
+	w := window.New(0, 0, 400, 300, "日期时间框", 0, xcc.Window_Style_Default)
 
 	dt := widget.NewDateTime(20, 50, 120, 26, w.Handle)
 	// 0为日期元素, 1为时间元素.
 	dt.SetStyle(0)
 
-	dt2 := widget.NewDateTime(20, 150, 120, 26, w.Handle)
+	dt2 := widget.NewDateTime(200, 50, 120, 26, w.Handle)
 	// 0为日期元素, 1为时间元素.
 	dt2.SetStyle(1)
 
