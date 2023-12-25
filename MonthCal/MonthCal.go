f2d9e7c98b85300c39e0096ca03a0f4373c76a97
twgh <twgh521@qq.com>
2023-12-25 22:23:45 +0800

    月历卡片

--- MonthCal/MonthCal.go
+++ MonthCal/MonthCal.go
@@ -1,4 +1,6 @@
 // 月历卡片
+// 要美化的话, 就得自绘, 看这个: http://www.xcgui.com/doc-ui/page_draw__month_cal.html
+// 我以后有空会翻译几个好看的: http://mall.xcgui.com/1618.html
 package main
 
 import (
@@ -12,14 +14,16 @@ import (
 
 func main() {
 	a := app.New(true)
-	w := window.New(0, 0, 400, 300, "", 0, xcc.Window_Style_Default)
+	a.EnableDPI(true)
+	a.EnableAutoDPI(true)
+	w := window.New(0, 0, 400, 300, "月历卡片", 0, xcc.Window_Style_Default)
 
 	// 创建MonthCal
 	monthCal := widget.NewMonthCal(30, 40, 290, 240, w.Handle)
 	// 注册月历元素日期改变事件
 	monthCal.Event_MONTHCAL_CHANGE(func(pbHandled *bool) int {
 		// 获取被选择的年月日
-		var pnYear, pnMonth, pnDay int
+		var pnYear, pnMonth, pnDay int32
 		monthCal.GetSelDate(&pnYear, &pnMonth, &pnDay)
 
 		fmt.Printf("%d年%d月%d日\n", pnYear, pnMonth, pnDay)
