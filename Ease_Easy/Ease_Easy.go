510b8e93681178efd9d4ebe5595043e7f662aaba
twgh <twgh521@qq.com>
2024-05-03 22:05:43 +0800

    简单缓动

--- Ease_Easy/Ease_Easy.go
+++ Ease_Easy/Ease_Easy.go
@@ -2,6 +2,7 @@
 package main
 
 import (
+	"runtime"
 	"time"
 
 	"github.com/twgh/xcgui/app"
@@ -17,25 +18,25 @@ var (
 )
 
 func main() {
+	// 这是必要的, 这将保证main函数中对UI库命令的调用是在一个系统线程中执行的。
+	// 如果不在一个系统线程中执行, 那程序有很大概率卡死.
+	runtime.LockOSThread()
+	defer runtime.UnlockOSThread()
+
 	a = app.New(true)
 	a.SetPaintFrequency(10)
 	w = window.New(0, 0, 400, 300, "", 0, xcc.Window_Style_Default)
 	w.ShowWindow(xcc.SW_SHOW)
 
-	time.AfterFunc(time.Millisecond*3, func() {
-		// 获取窗口坐标
-		var rect xc.RECT
-		w.GetRect(&rect)
-
-		// 缓动
-		for i := 1; i <= 30; i++ {
-			v := ease.Bounce(float32(i)/30.0, xcc.Ease_Type_Out)
-			y := int32(v * float32(rect.Top))
-
-			w.SetPosition(rect.Left, y)
-			time.Sleep(time.Millisecond * 10)
-		}
-	})
+	var rect xc.RECT
+	w.GetRect(&rect)
+	for t := 0; t <= 30; t++ {
+		v := ease.Bounce(float32(t)/30.0, xcc.Ease_Type_Out)
+		y := int32(v * float32(rect.Top))
+		w.SetPosition(rect.Left, y)
+		w.Redraw(true)
+		time.Sleep(time.Millisecond * 10)
+	}
 
 	a.Run()
 	a.Exit()
