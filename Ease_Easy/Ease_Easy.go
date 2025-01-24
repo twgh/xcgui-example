e482f448b49a8b014ea5861d668be3ee347cd248
twgh <twgh521@qq.com>
2025-01-24 19:21:46 +0800

    简单缓动

--- Ease_Easy/Ease_Easy.go
+++ Ease_Easy/Ease_Easy.go
@@ -8,7 +8,6 @@ import (
 	"github.com/twgh/xcgui/app"
 	"github.com/twgh/xcgui/ease"
 	"github.com/twgh/xcgui/window"
-	"github.com/twgh/xcgui/xc"
 	"github.com/twgh/xcgui/xcc"
 )
 
@@ -20,20 +19,22 @@ var (
 func main() {
 	// 这是必要的, 这将保证main函数中对UI库命令的调用是在一个系统线程中执行的。
 	// 如果不在一个系统线程中执行, 那程序有很大概率卡死.
+	// 因为下面用了time.Sleep(), go的运行时可能会进行调度, 就跳到其他线程了, 所以必须用这个.
 	runtime.LockOSThread()
 	defer runtime.UnlockOSThread()
 
 	a = app.New(true)
+	a.EnableAutoDPI(true).EnableDPI(true)
 	a.SetPaintFrequency(10)
-	w = window.New(0, 0, 400, 300, "", 0, xcc.Window_Style_Default)
-	w.ShowWindow(xcc.SW_SHOW)
 
-	var rect xc.RECT
-	w.GetRect(&rect)
+	w = window.New(0, 0, 400, 300, "窗口简单缓动", 0, xcc.Window_Style_Default)
+	w.Show(true)
+
+	rc := w.GetRectDPI()
 	for t := 0; t <= 30; t++ {
 		v := ease.Bounce(float32(t)/30.0, xcc.Ease_Type_Out)
-		y := int32(v * float32(rect.Top))
-		w.SetPosition(rect.Left, y)
+		y := int32(v * float32(rc.Top))
+		w.SetPosition(rc.Left, y)
 		w.Redraw(true)
 		time.Sleep(time.Millisecond * 10)
 	}
