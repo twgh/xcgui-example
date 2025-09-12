2a5afd217e9685231027a0fb682e7029730d725a
twgh <twgh521@qq.com>
2025-09-12 22:36:30 +0800

    形状文本

--- ShapeText/ShapeText.go
+++ ShapeText/ShapeText.go
@@ -2,6 +2,8 @@
 package main
 
 import (
+	"strings"
+
 	"github.com/twgh/xcgui/app"
 	"github.com/twgh/xcgui/font"
 	"github.com/twgh/xcgui/widget"
@@ -10,9 +12,12 @@ import (
 )
 
 func main() {
+	// 初始化界面库
+	app.InitOrExit()
 	a := app.New(true)
-	a.EnableDPI(true)
-	a.EnableAutoDPI(true)
+	a.EnableAutoDPI(true).EnableDPI(true)
+
+	// 创建窗口
 	w := window.New(0, 0, 500, 500, "ShapeText", 0, xcc.Window_Style_Default)
 
 	st := widget.NewShapeText(15, 35, 100, 30, "测试字体大小", w.Handle)
@@ -25,9 +30,11 @@ func main() {
 	// 设置个新字体， 粗体
 	// st.SetFont(font.NewEX("幼圆", 50, xcc.FontStyle_Bold).Handle)
 
-	shapeText := widget.NewShapeText(15, 235, 150, 30, "测试文字自动换行测试文字自动换行测试文字自动换行测试文字自动换行测试文字自动换行测试文字自动换行测试文字自动换行测试文字自动换行", w.Handle)
-	shapeText.SetTextAlign(xcc.TextAlignFlag_Left | xcc.TextAlignFlag_Top) // 置文本对齐方式
-	shapeText.LayoutItem_SetHeight(xcc.Layout_Size_Auto, -1)               // 高度自动
+	shapeText := widget.NewShapeText(15, 235, 150, 30, strings.Repeat("测试文字自动换行", 10), w.Handle)
+	// 置文本对齐方式
+	shapeText.SetTextAlign(xcc.TextAlignFlag_Left | xcc.TextAlignFlag_Top)
+	// 高度自动
+	shapeText.LayoutItem_SetHeight(xcc.Layout_Size_Auto, -1)
 
 	w.Show(true)
 	a.Run()
