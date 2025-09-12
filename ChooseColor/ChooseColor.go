f1004a141761cb307368e95b4ac5015a93fb132c
twgh <twgh521@qq.com>
2025-09-12 22:29:32 +0800

    颜色选择器

--- ChooseColor/ChooseColor.go
+++ ChooseColor/ChooseColor.go
@@ -3,8 +3,9 @@ package main
 
 import (
 	"fmt"
-	"github.com/twgh/xcgui/xc"
 	"unsafe"
+
+	"github.com/twgh/xcgui/xc"
 
 	"github.com/twgh/xcgui/app"
 	"github.com/twgh/xcgui/wapi"
@@ -13,28 +14,30 @@ import (
 	"github.com/twgh/xcgui/xcc"
 )
 
-var w *window.Window
-var custColors [16]uint32 // 保存自定义颜色的数组
+var (
+	w          *window.Window
+	custColors [16]uint32 // 保存自定义颜色的数组
+)
 
 func main() {
+	// 初始化界面库
+	app.InitOrExit()
 	a := app.New(true)
-	a.EnableDPI(true)
-	a.EnableAutoDPI(true)
+	a.EnableAutoDPI(true).EnableDPI(true)
+
 	w = window.New(0, 0, 430, 300, "选择颜色", 0, xcc.Window_Style_Default)
 
-	widget.NewButton(20, 40, 100, 30, "选择颜色", w.Handle).Event_BnClick(onBnClick)
+	widget.NewButton(20, 40, 100, 30, "选择颜色", w.Handle).Event_BnClick(func(pbHandled *bool) int {
+		// 或调用封装好的函数: wutil.ChooseColor(w.Handle)
+		ExampleChooseColor()
+		return 0
+	})
 
 	a.ShowAndRun(w.Handle)
 	a.Exit()
 }
 
-func onBnClick(pbHandled *bool) int {
-	// 可直接调用: wutil.ChooseColor(w.Handle)
-	ExampleChooseColorW()
-	return 0
-}
-
-func ExampleChooseColorW() {
+func ExampleChooseColor() {
 	cc := wapi.ChooseColor{
 		LStructSize:    36,
 		HwndOwner:      w.GetHWND(),
@@ -48,7 +51,7 @@ func ExampleChooseColorW() {
 	}
 	cc.LStructSize = uint32(unsafe.Sizeof(cc))
 	ret := wapi.ChooseColorW(&cc)
-	fmt.Println(ret)
+	fmt.Println("选择颜色:", ret)
 	if !ret {
 		return
 	}
@@ -57,7 +60,7 @@ func ExampleChooseColorW() {
 	rgba := xc.RGB2RGBA(int(rgb), 255)
 	fmt.Println("rgb颜色:", rgb)
 	fmt.Println("炫彩使用的颜色:", rgba)
-	fmt.Println(custColors) // 如果你添加了自定义颜色, 会保存在这个数组里面, 然后只要这个数组还在, 再次打开选择颜色界面时, 之前添加的自定义颜色还会存在
+	fmt.Println("自定义颜色:", custColors) // 如果你添加了自定义颜色, 会保存在这个数组里面, 然后只要这个数组还在, 再次打开选择颜色界面时, 之前添加的自定义颜色还会存在
 
 	// 设置窗口背景颜色
 	w.AddBkFill(xcc.Window_State_Flag_Leave, rgba)
