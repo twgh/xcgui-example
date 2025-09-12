f8d9ab70b4d547fe64d2c24da1c5639f52c512d1
twgh <twgh521@qq.com>
2025-09-12 22:38:20 +0800

    时钟

--- Timer/Timer.go
+++ Timer/Timer.go
@@ -2,32 +2,30 @@
 package main
 
 import (
+	"time"
+
 	"github.com/twgh/xcgui/app"
 	"github.com/twgh/xcgui/font"
 	"github.com/twgh/xcgui/widget"
 	"github.com/twgh/xcgui/window"
 	"github.com/twgh/xcgui/xcc"
-	"time"
-)
-
-var (
-	a    *app.App
-	w    *window.Window
-	text *widget.ShapeText
 )
 
 func main() {
-	a = app.New(true)
-	a.EnableDPI(true)
-	a.EnableAutoDPI(true)
-	w = window.New(0, 0, 500, 130, "定时器", 0, xcc.Window_Style_Default)
+	// 初始化界面库
+	app.InitOrExit()
+	a := app.New(true)
+	a.EnableAutoDPI(true).EnableDPI(true)
 
-	text = widget.NewShapeText(50, 50, 400, 50, time.Now().Format("2006-01-02 15:04:05"), w.Handle)
+	// 创建窗口
+	w := window.New(0, 0, 500, 130, "定时器", 0, xcc.Window_Style_Default)
+
+	text := widget.NewShapeText(50, 50, 400, 50, time.Now().Format("2006-01-02 15:04:05"), w.Handle)
 	text.SetFont(font.New(30).Handle)
 
 	// 定时器id是自己定的
 	w.SetTimer(1, 1000)
-	w.Event_TIMER(func(nIDEvent uint, pbHandled *bool) int {
+	w.AddEvent_Timer(func(hWindow int, nIDEvent uint, pbHandled *bool) int {
 		switch nIDEvent {
 		case 1:
 			text.SetText(time.Now().Format("2006-01-02 15:04:05"))
@@ -35,6 +33,8 @@ func main() {
 		}
 		return 0
 	})
+	// 窗口_关闭定时器
+	// w.KillTimer(1)
 
 	w.Show(true)
 	a.Run()
