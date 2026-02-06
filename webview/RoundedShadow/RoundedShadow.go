f76ef769face2db68cf83663fd6f54916e8b7703
twgh <twgh521@qq.com>
2026-02-06 23:33:39 +0800

    圆角阴影窗口

---
+++ webview/RoundedShadow/RoundedShadow.go
@@ -0,0 +1,236 @@
+// 圆角阴影窗口
+package main
+
+import (
+	"embed"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/twgh/xcgui/app"
+	"github.com/twgh/xcgui/edge"
+	"github.com/twgh/xcgui/wapi"
+	"github.com/twgh/xcgui/window"
+	"github.com/twgh/xcgui/xc"
+	"github.com/twgh/xcgui/xcc"
+)
+
+var (
+	isDebug = true // 是否为调试版
+
+	//go:embed assets/**
+	embedAssets embed.FS // 嵌入 assets 目录以及子目录下的文件, 不包括隐藏文件
+)
+
+const hostName = "app.example"
+
+type MainWindow struct {
+	edg *edge.Edge
+	w   *window.Window
+	wv  *edge.WebView
+}
+
+func NewMainWindow(edg *edge.Edge) *MainWindow {
+	m := &MainWindow{edg: edg}
+	var err error
+
+	// 创建 WebView
+	m.w, m.wv, err = m.edg.NewWebViewWithWindow(
+		edge.WithXmlWindowTitle("圆角阴影窗口"),
+		edge.WithXmlWindowSize(900, 600),
+		edge.WithFillParent(true),
+		edge.WithAppDrag(true),
+		edge.WithAutoFocus(true),
+		// 设置默认背景色为透明
+		edge.WithDefaultBackgroundColor(edge.NewColor(0, 0, 0, 0)),
+		edge.WithDebug(isDebug),
+		edge.WithDefaultContextMenus(isDebug),
+		edge.WithBrowserAcceleratorKeys(isDebug),
+		edge.WithStatusBar(false),
+		edge.WithZoomControl(false),
+	)
+	if err != nil {
+		wapi.MessageBoxW(0, "创建 webview 失败: "+err.Error(), "错误", wapi.MB_OK|wapi.MB_IconError)
+		os.Exit(1)
+	}
+
+	// 设置为透明窗口
+	m.w.SetTransparentType(xcc.Window_Transparent_Shaped)
+	// 窗口_置最小大小
+	m.w.SetMinimumSize(400, 400)
+
+	if !isDebug { // 正式版采用嵌入文件系统的方式
+		// 设置虚拟主机名和嵌入文件系统之间的映射
+		err = edge.SetVirtualHostNameToEmbedFSMapping(hostName, embedAssets)
+		if err != nil {
+			wapi.MessageBoxW(0, "SetVirtualHostNameToEmbedFSMapping 失败: "+err.Error(), "错误", wapi.MB_OK|wapi.MB_IconError)
+			os.Exit(6)
+		}
+		// 启用虚拟主机名和嵌入文件系统之间的映射
+		err = m.wv.EnableVirtualHostNameToEmbedFSMapping(true)
+		if err != nil {
+			wapi.MessageBoxW(0, "EnableVirtualHostNameToEmbedFSMapping 失败: "+err.Error(), "错误", wapi.MB_OK|wapi.MB_IconError)
+			os.Exit(5)
+		}
+	} else { // 调试版采用文件夹映射的方式方便热重载
+		folderPath, _ := filepath.Abs("webview/RoundedShadow/assets")
+		err = m.wv.SetVirtualHostNameToFolderMapping(hostName,
+			folderPath, edge.COREWEBVIEW2_HOST_RESOURCE_ACCESS_KIND_ALLOW)
+		if err != nil {
+			wapi.MessageBoxW(0, "SetVirtualHostNameToFolderMapping 失败: "+err.Error(), "错误", wapi.MB_OK|wapi.MB_IconError)
+			os.Exit(5)
+		}
+		// 注意: 并不会自动热重载, 需要手动刷新页面.
+		// 由于只是简单的静态页面, 可以让 AI 实现一个, 很简单的, 比如用 https://github.com/fsnotify/fsnotify 监听前端文件变化, 然后刷新页面.
+		// 或者查看 VueAndVite 例子.
+	}
+
+	// 绑定函数
+	m.bindWindowFuncs()
+
+	// 注册事件
+	m.regWebViewEvents()
+	m.regXcEvents()
+
+	// 导航到首页
+	m.wv.Navigate(edge.JoinUrlHeader(hostName) + "/RoundedShadow.html")
+	m.w.Show(true)
+	return m
+}
+
+func (m *MainWindow) regWebViewEvents() {
+	var firstLoad = true
+	// 导航完成事件
+	m.wv.Event_NavigationCompleted(func(sender *edge.ICoreWebView2, args *edge.ICoreWebView2NavigationCompletedEventArgs) uintptr {
+		uri := sender.MustGetSource()
+		fmt.Println("导航完成:", uri)
+
+		switch uri {
+		case edge.JoinUrlHeader(hostName) + "/RoundedShadow.html":
+			// 在导航完成事件里判断第一次加载完毕时才显示窗口,
+			// 这是因为采用嵌入文件系统的方式时, 网页还没加载出来的时候, 会显示webview白色的背景,
+			// 然后才会加载出网页, 表现出来就是有一瞬间的闪烁, 所以等加载完再显示窗口
+			if firstLoad {
+				firstLoad = false
+				m.w.Show(true)
+			}
+		}
+		return 0
+	})
+}
+
+// 炫彩事件
+func (m *MainWindow) regXcEvents() {
+	// wasMinimized 窗口是否最小化
+	var wasMinimized = false
+
+	// 窗口消息过程事件
+	m.w.AddEvent_WindProc(func(hWindow int, message uint32, wParam, lParam uintptr, pbHandled *bool) int {
+		switch message {
+		case wapi.WM_SIZE:
+			switch wParam {
+			case wapi.SIZE_MINIMIZED: // 窗口最小化
+				wasMinimized = true
+			case wapi.SIZE_MAXIMIZED: // 窗口最大化
+				wasMinimized = false
+				m.wv.Eval(`enableWindowShadow(false);`) // 禁用窗口阴影和圆角
+			case wapi.SIZE_RESTORED: // 窗口被还原（注意：也包括程序启动时的初始显示）
+				if wasMinimized { // 从最小化状态还原
+					wasMinimized = false
+				} else {
+					m.wv.Eval(`enableWindowShadow(true);`) // 启用窗口阴影和圆角
+				}
+			}
+		}
+		return 0
+	})
+}
+
+// 绑定窗口函数.
+func (m *MainWindow) bindWindowFuncs() {
+	// 绑定 最小化窗口函数
+	m.wv.Bind("wnd.minimize", func() {
+		m.w.ShowWindow(xcc.SW_MINIMIZE)
+	})
+
+	// 绑定 切换最大化窗口函数
+	m.wv.Bind("wnd.toggleMaximize", func() {
+		m.w.MaxWindow(!m.w.IsMaxWindow())
+	})
+
+	// 绑定 关闭窗口函数
+	m.wv.Bind("wnd.close", func() {
+		m.w.CloseWindow()
+	})
+
+	// 绑定 设置窗口位置函数
+	m.wv.Bind("wnd.setPos", func(x, y int32) {
+		m.w.SetPosition(m.w.DpiConv(x), m.w.DpiConv(y))
+	})
+
+	// 绑定 设置窗口尺寸函数
+	m.wv.Bind("wnd.setSize", func(width, height int32) {
+		m.w.SetSize(width, height)
+	})
+
+	// 绑定 获取窗口矩形函数
+	m.wv.Bind("wnd.getRect", func() xc.RECT {
+		return m.w.GetRectEx()
+	})
+}
+
+func main() {
+	checkWebView2()
+	edg := createEdge()
+
+	// 初始化界面库
+	app.InitOrExit()
+	a := app.New(true)
+	// 启用自适应DPI
+	a.EnableAutoDPI(true).EnableDPI(true)
+
+	NewMainWindow(edg)
+
+	a.Run()
+	a.Exit()
+}
+
+func createEdge() *edge.Edge {
+	// 创建 webview 环境
+	edg, err := edge.New(edge.Option{
+		UserDataFolder: os.TempDir(), // 自己的软件应该在里面创建一个自己的目录, 而不是直接用临时目录
+		EnvOptions: &edge.EnvOptions{
+			DisableTrackingPrevention: true, // 禁用 WebView2 中的跟踪防护功能
+			ScrollBarStyle:            edge.COREWEBVIEW2_SCROLLBAR_STYLE_FLUENT_OVERLAY,
+		},
+	})
+	if err != nil {
+		wapi.MessageBoxW(0, "创建 webview 环境失败: "+err.Error(), "错误", wapi.MB_OK|wapi.MB_IconError)
+		os.Exit(1)
+	}
+	return edg
+}
+
+func checkWebView2() {
+	// 高版本运行时是兼容低版本的。低版本不用高版本的方法也没有问题。
+	fmt.Println("本库使用的 WebView2 运行时版本号:", edge.GetVersion())
+	// 获取本机已安装的 WebView2 运行时版本号
+	localVersion, err := edge.GetAvailableBrowserVersion()
+	if err != nil {
+		wapi.MessageBoxW(0, "获取 WebView2 运行时版本号失败: "+err.Error(), "提示", wapi.MB_IconError)
+		os.Exit(1)
+	}
+	if localVersion == "" {
+		wapi.MessageBoxW(0, "请安装 WebView2 运行时后再打开程序! 下载完后请使用管理员权限运行安装包!", "提示", wapi.MB_IconWarning|wapi.MB_OK)
+		// 使用默认浏览器打开 WebView2 运行时小型引导程序下载地址.
+		edge.DownloadWebView2()
+		os.Exit(2)
+	}
+	fmt.Println("本机安装的 WebView2 运行时版本号:", localVersion)
+
+	// 判断本机 WebView2 运行时版本是否低于本库使用的 WebView2 运行时版本
+	if ret, _ := edge.CompareBrowserVersions(localVersion, edge.GetVersion()); ret == -1 {
+		log.Println("本机 WebView2 运行时版本低于本库使用的 WebView2 运行时版本!")
+	}
+}
